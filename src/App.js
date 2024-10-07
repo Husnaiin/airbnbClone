d07b24c53a98f2d8ec6abfcd2f3146e2767aced3
Husnaiin <husnaiinahmad@gmail.com>
2024-10-07 23:43:01 +0500

    add horizontal filter bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
-
+import { useState } from "react";
+import Filter from "./components/Filter";
 import Header from "./components/Header";
-
 function App() {
+  const [selectedFilter, setSelectedFilter] = useState(0);
   return (
     <div className="App">
       <Header />
-    
+      <Filter
+        selectedFilter={selectedFilter}
+        setSelectedFilter={setSelectedFilter}
+      />
     </div>
   );
 }
